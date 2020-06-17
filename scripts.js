c1436bf3eec9a1cab400febb56969f2e2cab957d
Raven19931125@mail.ru <DC1993universe>
2020-06-17 18:31:39 +0300

    five commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,27 @@
-$('.burger').on('click', function () {
-    $('.menu').addClass('open');
+$(document).ready(function () {
+
+    $('.burger').on('click', function () {
+        $('.menu').addClass('open');
+    });
+    $('.closed-menu').on('click', function () {
+        $('.menu').removeClass('open');
+    })
+
+    $('.home-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        items:1
+    });
+    $('.reviews-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        items:1
+    });
+    $('.owl-dot').each(function(){
+        $(this).children('span').text($(this).index()+1);
+    });
 });
-$('.closed-menu').on('click', function () {
-    $('.menu').removeClass('open');
-})
